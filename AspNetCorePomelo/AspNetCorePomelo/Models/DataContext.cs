30cbccad615314bc9e9f5a7499d08162536724f2
Daniel Rosa da Silveia <danielrosasilveira@gmail.com>
2020-01-09 12:01:47 -0300

    Definição de DBContext e ConnectionString

---
+++ AspNetCorePomelo/AspNetCorePomelo/Models/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace AspNetCorePomelo.Models
+{
+    public class DataContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json");
+            var configuration = builder.Build();
+            optionsBuilder.UseMySql(configuration["ConnectionString:DefaultConnetion"]);
+        }
+
+        public DbSet<Usuarios> Usuarios { get; set; }
+    }
+}
